aa183dbf6009e3c1bb774cc34d367430e939c120
Neill Erasmus <s227265041@mandela.ac.za>
2026-04-16 15:52:46 +0200

    Inserted tutorial citation code.

---
+++ tools/citation.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+def format_citation(url: str, title: str, author: str = 'Unknown') -> str:
+    """ Formats a basic APA-style citation. """
+    today = date.today()
+    year = today.year
+    accessed = today.strftime('%B %d, %Y')
+    return ( f'{author}. ({year}). {title}. ' f'Retrieved {accessed}, from {url}' )
